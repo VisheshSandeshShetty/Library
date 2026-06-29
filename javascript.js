79f042a3e93e617d42d75f542f1d8e3b518b1135
VisheshSandeshShetty <visheshshetty33@gmail.com>
2026-06-29 07:56:54 +0000

    Personal Library Record Implemented with Add, Delete and Modify Status options. However, the website does not have persistant storage by choice for the moment

--- javascript.js
+++ javascript.js
@@ -7,6 +7,7 @@ function Book(title,author,pages,read){
     this.author=author;
     this.pages=pages;
     this.read=read;
+    this.id=crypto.randomUUID();
 }
 
 function addBooktoLibrary(book){
@@ -15,9 +16,20 @@ function addBooktoLibrary(book){
 }
 const b1=new Book("The Hobbit", "J.R.R. Tolkien", 295, "unread");
  addBooktoLibrary(b1);
-
- const b2=new Book("The Basir", "Asir", 420, "read");
- addBooktoLibrary(b2);
+const b2 = new Book("Meditations", "Marcus Aurelius", 304, "read");
+addBooktoLibrary(b2);
+const b3 = new Book("Tao Te Ching", "Lao Tzu", 184, "read");
+addBooktoLibrary(b3);
+const b4 = new Book("Dancing with Siva", "Satguru Sivaya Subramuniyaswami", 1008, "unread");
+addBooktoLibrary(b4);
+const b5 = new Book("The Art of War", "Sun Tzu", 273, "read");
+addBooktoLibrary(b5);
+const b6 = new Book("Atomic Habits", "James Clear", 320, "unread");
+addBooktoLibrary(b6);
+const b7 = new Book("1984", "George Orwell", 328, "read");
+addBooktoLibrary(b7);
+const b8 = new Book("The Alchemist", "Paulo Coelho", 208, "unread");
+addBooktoLibrary(b8);
 
 console.log();
 console.log("Books in Library: ");
@@ -26,6 +38,7 @@ myLibrary.forEach(function(book){
 })
 
 const LibraryDiv=document.getElementById("library");
+
 function displayBooks(){
     LibraryDiv.innerHTML="";
     myLibrary.forEach(function(book){
@@ -34,8 +47,74 @@ function displayBooks(){
         <h2>${book.title}</h2>
         <p><strong>Author: </strong>${book.author}</p>
         <p><strong>Pages: </strong>${book.pages}</p>
-        <p><strong>Status: </strong>${book.read}</p>
-        ----------------------------------------<br>`;
+        <p><strong>Status: 
+            <span style="color:${book.read=="read"?"green":"red"}">
+            ${book.read}</strong>
+            </span>    
+        </p>
+        <p><strong>Book ID: </strong>${book.id}</p>
+        <br>
+        <button class="remove-book-btn" data-id="${book.id}">Delete</button> 
+        <button class="status-btn" data-id="${book.id}">Change Status</button>
+        <br>
+        -----------------------------------------------------------------<br>`;
     })
+
+    const statusBtns=document.querySelectorAll(".status-btn");
+    statusBtns.forEach(function(button){
+        button.addEventListener("click", function(){
+            const id=button.dataset.id;
+            const book=myLibrary.find(function(book){
+                return book.id==id;
+            })
+        book.toggleRead();
+        displayBooks();
+        })
+    })
+
+    const deleteBtns=document.querySelectorAll(".remove-book-btn");
+    deleteBtns.forEach(function(button){
+        button.addEventListener("click", function(){
+            const id=button.dataset.id;
+            const idx=myLibrary.findIndex(function(book){
+                return book.id==id;
+            })
+            myLibrary.splice(idx,1);
+            displayBooks();
+        })
+    })
+
 }
+
+
+const newBookBtn=document.getElementById("new-book-btn");
+const bookDialog=document.getElementById("new-book-dialog");
+newBookBtn.addEventListener("click", function(){
+    bookDialog.showModal();
+})
+
+const bookForm=document.getElementById("book-form");
+bookForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const title=document.getElementById("title").value;
+    const author=document.getElementById("author").value;
+    const pages=document.getElementById("pages").value;
+    const read=document.getElementById("read").value;
+
+    const newBook=new Book(title,author,pages,read);
+    addBooktoLibrary(newBook);
+    displayBooks();
+    bookDialog.close();
+    bookForm.reset(); 
+})
+
+const closeBtn=document.getElementById("close-dialog");
+closeBtn.addEventListener("click", function(){
+    bookDialog.close();
+})
+
+Book.prototype.toggleRead=function(){
+    this.read=this.read=="read"?"unread":"read";
+}
+
 displayBooks();
